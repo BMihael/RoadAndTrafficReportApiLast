ee7581a7a7d5afdfb912d92ccbce21e5361c55c1
BMihael <mihael.belko@windowslive.com>
2022-08-25 18:37:09 +0200

    Refactor JwtSecretHolder, added data anotation

--- src/main/java/com/example/ratra/util/JwtSecretHolder.java
+++ src/main/java/com/example/ratra/util/JwtSecretHolder.java
@@ -1,30 +1,14 @@
 package com.example.ratra.util;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix="spring.jwt")
+@Data
 //Ovdje sam našao ovu anotaciju https://docs.spring.io/spring-boot/docs/1.0.1.RELEASE/reference/html/boot-features-external-config.html
 public class JwtSecretHolder {
-
     private String secret;
     private int expiration;
-
-    public String getSecret() {
-        return secret;
-    }
-
-    public void setSecret(String secret) {
-        this.secret = secret;
-    }
-
-    public int getExpiration() {
-        return expiration;
-    }
-
-    public void setExpiration(int expiration) {
-        this.expiration = expiration;
-    }
 }
