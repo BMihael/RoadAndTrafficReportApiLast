f40d83246e0551a92956ab24cda16805bfec0817
BMihael <mihael.belko@windowslive.com>
2022-08-25 18:35:19 +0200

    Added class and interface for generic messages response

---
+++ src/main/java/com/example/ratra/model/response/ResponseMessages.java
@@ -0,0 +1,12 @@
+package com.example.ratra.model.response;
+
+public interface ResponseMessages {
+    String USER_REGISTER_SUCCESSFULLY = "User registered successfully!";
+    String USERNAME_TAKEN = "Username already taken!";
+    String USERNAME_NOT_FOUND = "Username not found!";
+    String EMAIL_TAKEN = "Email already taken!";
+    String IMAGE_DELETED = "Image Deleted!";
+    String IMAGE_NOT_FOUND = "Image not found!";
+    String LOCATION_NOT_FOUND = "Location with given id not found!";
+    String SUBMIT_NOT_FOUND = "Submit not found!";
+}
